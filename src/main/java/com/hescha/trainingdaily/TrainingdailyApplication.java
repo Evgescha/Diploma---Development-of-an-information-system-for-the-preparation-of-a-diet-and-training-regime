34246e2f18b31f1ec55a76ce40dba5a612a0b1eb
Yauhescha <evgescha.maslov007@yandex.ru>
2021-11-27 21:22:48 +0300

    Configs added, unusual method deleted, start message added

--- src/main/java/com/hescha/trainingdaily/TrainingdailyApplication.java
+++ src/main/java/com/hescha/trainingdaily/TrainingdailyApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TrainingdailyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TrainingdailyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingdailyApplication.class, args);
+        System.out.println("Application started!");
+    }
 
 }
--- src/main/java/com/hescha/trainingdaily/repository/UserRepository.java
+++ src/main/java/com/hescha/trainingdaily/repository/UserRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    public User findByUsernameIgnoreCase(String username);
-
-    public User findByUsername(String username);
+    User findByUsernameIgnoreCase(String username);
 }
